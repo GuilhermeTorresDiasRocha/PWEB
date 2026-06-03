2c541d04e716f6daca238fb467a2cfd43a0208d9
GTDR <professionalonlymail@duck.com>
2026-06-03 20:46:47 -0300

    Atividade17

---
+++ Atividade17/app.js
@@ -0,0 +1,24 @@
+let express=require('express');
+let app=express();//executando o express
+
+app.set('view engine','ejs');
+
+app.get('/', function (req,res){
+    res.render("home/index")
+});
+app.get('/formulario_adicionar_usuario', function (req,res){
+   res.render("admin/adicionar_usuarios");
+});
+app.get('/informacao/historia', function (req,res){
+   res.render("informacao/historia");
+});
+app.get('/informacao/cursos', function (req,res){
+    res.render("informacao/cursos");
+});
+app.get('/informacao/professores', function (req,res){
+    res.render("informacao/professores");
+});
+app.listen(3000,function(){
+    console.log("servidor com express foi carregado com sucesso");
+});
+
