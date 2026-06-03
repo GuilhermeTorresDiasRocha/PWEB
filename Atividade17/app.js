314e43f88d9a61007e2cda5e33c950f500eda15b
GTDR <professionalonlymail@duck.com>
2026-06-03 20:50:43 -0300

    Atividade 17 - Parcial

--- Atividade17/app.js
+++ Atividade17/app.js
@@ -1,24 +1,32 @@
-let express=require('express');
-let app=express();//executando o express
+const express = require("express");
 
-app.set('view engine','ejs');
+const app = express();
 
-app.get('/', function (req,res){
-    res.render("home/index")
-});
-app.get('/formulario_adicionar_usuario', function (req,res){
-   res.render("admin/adicionar_usuarios");
-});
-app.get('/informacao/historia', function (req,res){
-   res.render("informacao/historia");
-});
-app.get('/informacao/cursos', function (req,res){
-    res.render("informacao/cursos");
-});
-app.get('/informacao/professores', function (req,res){
-    res.render("informacao/professores");
-});
-app.listen(3000,function(){
-    console.log("servidor com express foi carregado com sucesso");
+const texto = require("./modulo1");
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.render("home");
 });
 
+app.get("/formulario-adicionar-usuario", (req, res) => {
+	res.render("admin/adicionar_usuario");
+});
+
+app.get("/historia", (req, res) => {
+	res.render("informacao/historia");
+});
+
+app.get("/cursos", (req, res) => {
+	res.render("informacao/cursos");
+});
+
+app.get("/professores", (req, res) => {
+	res.render("informacao/professores");
+});
+
+app.listen(3000, () => {
+	console.log("Server started!");
+	console.log(texto);
+});
